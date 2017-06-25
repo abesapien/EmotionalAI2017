7898d5647cf90763894b346df623bdd79ed316d1
Akinori Mitani <amitani.tky@gmail.com>
2017-06-24 23:30:23 -0700

    added test for thread, and added __enter__ and __exit__ to EDT

--- aki/emotion_detection_thread.py
+++ aki/emotion_detection_thread.py
@@ -11,6 +11,7 @@ from common import clock, draw_str
 from keras.models import load_model
 
 import threading
+import time
 
 class EmotionDetectionThread(threading.Thread):
     def detect(self, img, cascade):
@@ -32,37 +33,49 @@ class EmotionDetectionThread(threading.Thread):
         self.lock = threading.Lock()
         self.cascade = cv2.CascadeClassifier(self.cascade_fn)
         self.video_src = 0
+        self.interval=0.05
         self.outputs = []
+        self.cam = create_capture(self.video_src)
+        print('capture started\n')
+        self.running=True
 
     def run(self):
-        cam = create_capture(self.video_src)
-        model = load_model(self.model_path)
-        while True:
-            ret, img = cam.read()
+        print('running thread\n')
+        self.model = load_model(self.model_path)
+        print('model loaded\n')
+        while self.running:
+            print('running thread\n')
+            ret, img = self.cam.read()
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             gray = cv2.equalizeHist(gray)
             t = clock()
             rects = self.detect(gray, self.cascade)
             vis = img.copy()
-            self.draw_rects(vis, rects, (0, 255, 0))
-            outputs=[]
+            #self.draw_rects(vis, rects, (0, 255, 0))
+            outputs=[t]
             for x1, y1, x2, y2 in rects:
                 roi = gray[y1:y2, x1:x2]
                 input = cv2.resize(roi,(48,48))
-                output=model.predict(np.reshape(input,(1,48,48,1)))
+                output=self.model.predict_proba(np.reshape(input,(1,48,48,1)),verbose=0)
                 outputs.append(output)
                 vis_roi = vis[y1:y2, x1:x2]
                 emotions = ('Angry','Disgust','Fear','Happy','Sad','Surprise','Neutral')
-                draw_str(vis_roi, (20, 20),  emotions[output[0].argmax()])
+            #    draw_str(vis_roi, (20, 20),  emotions[output[0].argmax()])
             with self.lock:
-                if len(self.outputs)<10:
+                if len(self.outputs)<300:
                     self.outputs.append(outputs)
                 else:
                     self.outputs = self.outputs[1:] + outputs
 
             dt = clock() - t
-            draw_str(vis, (20, 20), 'time: %.1f ms' % (dt*1000))
-            cv2.imshow('facedetect', vis)
+            #draw_str(vis, (20, 20), 'time: %.1f ms' % (dt*1000))
+            #cv2.imshow('facedetect', vis)
+            #time.sleep(self.interval)
+    def __enter__(self):
+        self.start()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.running=False
 
     def __del__(self):
         cv2.destroyAllWindows()
