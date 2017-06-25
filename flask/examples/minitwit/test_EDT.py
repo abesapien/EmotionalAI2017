441e049cc7f82b5581a802c13075277507e5965b
shawn faison <abesapien@shawns-MacBook-Pro.local>
2017-06-25 12:57:28 -0700

    Cleaned up some files .

--- flask/examples/minitwit/test_EDT.py
+++
@@ -1,7 +0,0 @@
-import emotion_detection_thread
-import time
-edt=emotion_detection_thread.EmotionDetectionThread()
-with edt:
-    while True:
-        print edt.getEmotion()
-        time.sleep(1)
